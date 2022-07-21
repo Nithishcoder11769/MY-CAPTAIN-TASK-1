b0c4222684f7b08ae7b185b6b6c4ab5ab8555efb
Nithishcoder11769 <96300708+Nithishcoder11769@users.noreply.github.com>
2022-07-21 21:04:32 +0530

    filename.py

--- filename.py
+++ filename.py
@@ -1,3 +1,3 @@
-FILE_NAME=input("Enter the filename")
-f_extns=FILE_NAME.split(".")
-print("the extension of the file name is:",repr(f_extns[-1]))
+file_name=input("Enter the filename:")
+f_extns=file_name.split(".")
+print("the extension of the file name is:",f_extns[-1])
